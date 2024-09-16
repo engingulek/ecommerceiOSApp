817fa02dc19f0fa68c0193dd0f2c024e803836b1
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-09-16 21:38:58 +0300

    Created Dependency Kit

---
+++ Ecommerce/Ecommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ecommerce
+//
+//  Created by Engin Gülek on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kits/DomainKits/DependencyKits/DependencyKits/DependencyKits.h
@@ -0,0 +1,18 @@
+//
+//  DependencyKits.h
+//  DependencyKits
+//
+//  Created by Engin Gülek on 16.09.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyKits.
+FOUNDATION_EXPORT double DependencyKitsVersionNumber;
+
+//! Project version string for DependencyKits.
+FOUNDATION_EXPORT const unsigned char DependencyKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyKits/PublicHeader.h>
+
+
